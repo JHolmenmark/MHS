ab703ecf38b291e79dd5d6b91d26983fac848266
wondmagegn Ergano <won_tade@yahoo.com>
2018-12-10 22:30:18 +0100

    Added MHS_Bank

---
+++ MHS_Bank.java
@@ -0,0 +1,14 @@
+package MoneyHandling;
+
+public class MHS_Bank {
+    
+    private String bankAccount;
+    private double balance;
+    MHS_BankAccount MHSbank = new MHS_BankAccount();
+    String [][] banks = {{"1234", "Nordea"},
+                         {"1423", "Handelsbanken"},
+                         {"5531", "SEB"}, 
+                         {"99516", "Swedbank"}};
+    public MHS_Bank(){
+        
+    }
