6d360d52eeb20f421de907c12daa36097bda1e6f
wondmagegn Ergano <won_tade@yahoo.com>
2018-12-10 22:07:05 +0100

    Added MHS_Main

---
+++ MHS_Main.java
@@ -0,0 +1,8 @@
+
+package MoneyHandling;
+
+public class MHS_Main {
+    private static String bankAccount = "1234111222";
+    private static double price = 10.45;
+       
+public static void main(String []args){
